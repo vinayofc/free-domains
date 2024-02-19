241be0a2f7929b076bb13946e0b9f7525d35e02e
Vinay Chaudhary <97957688+vinayofc@users.noreply.github.com>
2024-02-20 01:35:53 +0530

    Create 2api.is-an.app.js

---
+++ domains/2api.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: '2API is an API Endpoint',
+  domain: 'is-an.app',
+  subdomain: '2api',
+  owner: {
+    email: 'vinaychaudhary.sps@gmail.com',
+  },
+  record: {
+    A: '3.82.63.0',
+  },
+  proxy: false,
+})
